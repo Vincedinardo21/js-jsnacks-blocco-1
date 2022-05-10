706c1490fb381ba1348a0cfc9874d068a96f0c1a
vince <vincedinardo21@gmail.com>
2022-05-10 17:37:30 +0200

    Seconda lista (sbagliata)

--- myscript.js
+++ myscript.js
@@ -1,9 +1,16 @@
 // Arrays
 var primoArray = ["Aniello", "Oronzo", "Loredano", "Teodomildo", "Ezechiele"];
 var secondoArray = ["Rosso", "Giallo"];
-const listaEst = document.querySelector("ul.esterna");
 
 for(let i = 0; i < primoArray.length; i++){
-    const elementoEst = `<li>${primoArray[i]}</li>`;
-    listaEst.innerHTML += elementoEst;
+    const ulEst = document.querySelector("ul.esterna");
+    const liEst = document.createElement("li");
+    liEst.append(primoArray[i]);
+    ulEst.append(liEst);
+    for(let j = 0; j < secondoArray.length; j++){
+        const ulInt = document.querySelector("ul.interna");
+        const liInt = document.createElement("li");
+        liInt.append(secondoArray[j]);
+        ulInt.append(liInt);
+    }
 }
